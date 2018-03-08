f0733088ddd25942a4746c0de718e3645018df14
wrx <wrx_lyn@126.com>
2018-03-08 22:38:57 +0800

    springbootdemo

---
+++ spring-boot-cloud-parent/spring-boot-hello/src/main/java/com/lyn/HelloAppicationMain.java
@@ -0,0 +1,13 @@
+package com.lyn;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloAppicationMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloAppicationMain.class,args);
+    }
+}
--- src/main/java/com/lyn/SpringBootAppilcation.java
+++
@@ -1,7 +0,0 @@
-package com.lyn;
-
-public class SpringBootAppilcation {
-    public static void main(String[] args) {
-        System.out.println("helloWorld");
-    }
-}
